20e1c0732d79acb059e0e0cf22ee3dbf637bbf76
Olesyazfc <lesyi2001@mail.ru>
2022-11-15 22:58:38 +0600

    Correction

--- menu/task.js
+++ menu/task.js
@@ -1,12 +1,12 @@
 let linksArr = Array.from(document.querySelectorAll('.menu__link'))
 
 for(let i = 0; i < linksArr.length; i++) {
-    const link = links_arr[i]
+    const link = linksArr[i]
 
     link.onclick = () => {
         const parent = link.closest('.menu__item')
         if (parent.querySelector('.menu_sub') ) {
-            parent.querySelector('.menu_sub').classList.add('menu_active')
+            parent.querySelector('.menu_sub').classList.toggle('menu_active')
         };
     
         if (parent.closest('.menu_main') && parent.querySelector('ul')) {
--- slider/task.js
+++ slider/task.js
@@ -1,24 +1,27 @@
-const imgArr = Array.from(document.querySelectorAll('.slider__image'))
-
-let i = 0
+const imgArr = Array.from(document.querySelectorAll('.slider__item'))
+const left = document.querySelector('.slider__arrow_prev')
+const right = document.querySelector('.slider__arrow_next')
 
 right.onclick = () => {
-    imgArr[i].closest('.slider__item').className = ('slider__item')
-    i++
 
-    if (imgArr[i] == undefined) {
-        i = 0
-    }
-    imgArr[i].closest('.slider__item').classList.add('slider__item_active')
-};
+    const active = document.querySelector('.slider__item_active')
+    
+    let index = imgArr.findIndex(i => i == active)
+
+    index < imgArr.length - 1 ? ( index += 1 ) : ( index = 0 )
+
+    active.className = 'slider__item',
+    imgArr[index].classList.add('slider__item_active')
+}
 
 left.onclick = () => {
 
-    imgArr[i].closest('.slider__item').className = ('slider__item')
+    const active = document.querySelector('.slider__item_active')
+    
+    let index = imgArr.findIndex(i => i == active)
 
-    if (i == 0) {
-        i = imgArr.length - 1
-    }
-    i--
-    imgArr[i].closest('.slider__item').classList.add('slider__item_active')
-};
+    index > 0 ? ( index -= 1 ) : ( index = imgArr.length - 1 )
+
+    active.className = 'slider__item',
+    imgArr[index].classList.add('slider__item_active')
+}
